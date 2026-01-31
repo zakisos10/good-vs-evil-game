a142b717bc59c49e18abfa07415f9e16f06ebe16
zakisos10 <zakisos100@gmail.com>
2026-01-31 01:22:26 +0100

    Update script.js

---
+++ لعبة/public/script.js
@@ -0,0 +1,33 @@
+body {
+  background: #111;
+  color: white;
+  font-family: Arial;
+  text-align: center;
+  padding: 20px;
+}
+
+input {
+  padding: 10px;
+  margin: 5px;
+  font-size: 16px;
+  width: 200px;
+}
+
+button {
+  padding: 10px 20px;
+  font-size: 16px;
+  margin: 10px;
+  cursor: pointer;
+}
+
+#role {
+  font-size: 40px;
+  margin-top: 30px;
+  padding: 20px;
+  border-radius: 10px;
+}
+
+ul {
+  list-style: none;
+  padding: 0;
+}
